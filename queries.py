4ee10b5d8807f6f78f5352d78cdcfb1520ab7233
jennshn <75039453+jennshn@users.noreply.github.com>
2021-05-26 17:41:04 +0200

    Update queries.py

--- queries.py
+++ queries.py
@@ -1,6 +1,6 @@
-import jsonify, requests
+import jsonify, requests 
 from influxdb import InfluxDBClient
-import datetime
+import datetime #jsonify and datetime NOT needed for code
 
 client=InfluxDBClient(host='132.199.123.229', port=54683, username='prosem', password='prosem21kp', database='sensor_data')
 
